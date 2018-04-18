666423d7b0974405ebdce806608999005fa72e35
Jari Germeraad <jarinoes@gmail.com>
2018-04-18 13:25:24 +0200

    ENH: Code improvements

--- src/main/scala/net/atos/scalability/AkkaParallelismExample.scala
+++ src/main/scala/net/atos/scalability/AkkaParallelismExample.scala
@@ -12,12 +12,15 @@ import scala.concurrent.ExecutionContext
 object AkkaParallelismExample extends App {
   implicit val system: ActorSystem = ActorSystem("Parallelism")
   implicit val ec: ExecutionContext = system.dispatcher
+  implicit val materializer: ActorMaterializer = ActorMaterializer()
 
   val subjects = List.fill(8)("What is the name of the president of the United States?")
-  val flow = Flow[String] map (TextSubject(_)) via TermFrequencyScript.flow map (_.json)
+  val flow = (Flow[String] map (TextSubject(_)))
+    .via(TermFrequencyScript.flow)
+    .map(_.json)
 
   Source(subjects)
     .via(flow.parallel(4))
-    .runForeach(println)(ActorMaterializer())
+    .runForeach(println)
     .onComplete(_ => system.terminate)
 }
