290f91f7c7f32d182113741449664b1895f369a4
Jari Germeraad <jarinoes@gmail.com>
2018-06-05 11:31:50 +0200

    ENH: Configuration options

--- src/main/scala/net/atos/scalability/Configuration.scala
+++ src/main/scala/net/atos/scalability/Configuration.scala
@@ -1,21 +1,29 @@
 package net.atos.scalability
 
 case class Configuration(numberOfWorkers: Int,
-                         endpoint: (String, Int),
+                         httpPort: Int,
+                         akkaAddress: (String, Int),
+                         dockerIp: String,
                          seedNodes: List[String])
 
 object Configuration {
   type ConfigurationArgumentParser = (Configuration, Array[String]) => Configuration
   val DEFAULT_NUMBER_OF_WORKERS: Int = 1
-  val DEFAULT_IP = "0.0.0.0"
-  val DEFAULT_PORT: Int = System.getenv("port").toInt
+  val DEFAULT_PORT: Int = 8080
+  val DEFAULT_AKKA_IP = "127.0.0.1"
+  val DEFAULT_AKKA_PORT: Int = DEFAULT_PORT + 1
+  val DEFAULT_DOCKER_IP = "172.18.0.2"
   private val seedNodeRegex =
-    "akka\\.tcp:\\/\\/[a-zA-Z0-9]+@(?:\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}|[a-z-]+):\\d{2,6}".r
+    "akka\\.tcp:\\/\\/[a-zA-Z0-9]+@\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}:\\d{2,6}".r
   private val acceptedArguments: Map[String, ConfigurationArgumentParser] = Map(
     "--workers" -> parseNumberOfWorkers,
     "--seed-nodes" -> parseSeedNodes,
+    "--port" -> parsePort,
+    "--akka-host" -> parseAkkaHost,
+    "--akka-port" -> parseAkkaPort,
+    "--docker-ip" -> parseDockerIp,
     "-w" -> parseNumberOfWorkers,
-    "-s" -> parseSeedNodes,
+    "-s" -> parseSeedNodes
   )
 
   def from(args: Array[String]): Configuration = parse(groupArgumentsAndValues(args))
@@ -51,7 +59,12 @@ object Configuration {
     loop(arguments, Configuration.default)
   }
 
-  def default = Configuration(DEFAULT_NUMBER_OF_WORKERS, (DEFAULT_IP, DEFAULT_PORT), Nil)
+  def default = Configuration(
+    DEFAULT_NUMBER_OF_WORKERS,
+    DEFAULT_PORT,
+    (DEFAULT_AKKA_IP, DEFAULT_AKKA_PORT),
+    DEFAULT_DOCKER_IP,
+    Nil)
 
   private def parseNumberOfWorkers(configuration: Configuration, values: Array[String]): Configuration = {
     val numberOfWorkers = values.head.toInt
@@ -67,6 +80,34 @@ object Configuration {
     })
 
     configuration.copy(seedNodes = values.toList)
+  }
+
+  private def parsePort(configuration: Configuration, value: Array[String]): Configuration = value.headOption match {
+    case None => configuration
+    case Some(port) =>
+      configuration.copy(httpPort = port.toInt)
+  }
+
+  private def parseAkkaHost(configuration: Configuration, value: Array[String]): Configuration = value.headOption match {
+    case None => configuration
+    case Some(host) =>
+      val (_, port) = configuration.akkaAddress
+
+      configuration.copy(akkaAddress = (host, port))
+  }
+
+  private def parseAkkaPort(configuration: Configuration, value: Array[String]): Configuration = value.headOption match {
+    case None => configuration
+    case Some(port) =>
+      val (host, _) = configuration.akkaAddress
+
+      configuration.copy(akkaAddress = (host, port.toInt))
+  }
+
+  private def parseDockerIp(configuration: Configuration, value: Array[String]): Configuration = value.headOption match {
+    case None => configuration
+    case Some(host) =>
+      configuration.copy(dockerIp = host)
   }
 
 }
